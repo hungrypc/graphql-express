910847b04287cdc98140b7243ccf99c4474d6ff4
hpc <philipchanhk626@gmail.com>
2020-07-01 15:51:38 -0400

    added axios, making calls to db, able to fetch data with graphql

--- notes.md
+++ notes.md
@@ -5,3 +5,5 @@ start server:
 node server.js
 ```
 
+### Async Resolve Functions
+Resolve can also return a promise. GraphQL automatically detects that we return a promise, waits for the promise to resolve, then sends a response with the data to the user.
--- schema/schema.js
+++ schema/schema.js
@@ -5,12 +5,8 @@ const {
   GraphQLInt,
   GraphQLSchema
 } = graphql
-const _ = require('lodash')
+const axios = require('axios')
 
-const users = [
-  { id: '23', name: 'Bill', age: 20 },
-  { id: '13', name: 'Bob', age: 21 },
-]
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -32,7 +28,8 @@ const RootQuery = new GraphQLObjectType({
         }
       },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id })
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+        .then(response => response.data)
       }
     }
   }
