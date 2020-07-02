8abd86eff648fdef3bc257eb7123f1f74f9f97c4
hpc <philipchanhk626@gmail.com>
2020-07-02 15:40:11 -0400

    added Company Type, hooked up to User Type for nested queries

--- schema/schema.js
+++ schema/schema.js
@@ -7,13 +7,28 @@ const {
 } = graphql
 const axios = require('axios')
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  }
+})
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: { 
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+        .then(response => response.data)
+      }
+    }
   }
 })
 
